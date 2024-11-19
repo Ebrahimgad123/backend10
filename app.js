b7172ea6c7f8ccfc39d5523342e910a64fbc120b
egymatador2024 <egymatador2024@gmail.com>
2024-11-19 19:39:25 +0200

    first commit

--- app.js
+++ app.js
@@ -45,16 +45,18 @@ app.use(helmet()); // إضافة أمان بواسطة Helmet
 // إعداد الجلسات مع Passport
 const MongoStore = require('connect-mongo');
 app.use(session({
-  secret: 'Secret',
+  secret: 'your-secret-key',
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
   cookie: {
-    secure: false, // تأكد من أن secure:true في بيئة الإنتاج (HTTPS)
-    httpOnly: true, // حماية من الوصول للكوكيز عبر JavaScript
-    maxAge: 1000 * 60 * 60 * 24 // مدة صلاحية الـ session
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // استخدم secure إذا كان الخادم في بيئة الإنتاج
+    sameSite: 'None' // لضمان أن الـ cookies تعمل عبر النطاقات المختلفة
   },
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
 }));
+
+ 
 
 
 // إعداد CORS للسماح بالطلبات من الواجهة الأمامية
