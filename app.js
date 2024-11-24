c91b0b2131307a6984a07f999a0289b8221656f3
egymatador2024 <egymatador2024@gmail.com>
2024-11-24 10:18:29 +0200

    first commit

--- app.js
+++ app.js
@@ -1,62 +1,3 @@
-// const express = require("express");  //✔
-// const cors = require("cors");       //✔
-// const compression = require("compression"); 
-// const path = require("path");   
-// const helmet = require("helmet");
-// require("dotenv").config(); 
-// const dbConfig = require("./config/db"); 
-// const { logger } = require("./middlewares/logger"); 
-// const { notFound, errorHandling } = require("./middlewares/errorHandler");
-// const mongoose = require('mongoose');
-// const passport = require('passport');
-// const session = require('express-session');
-// const cookieSession=require('cookie-session')
-// const cookieParser = require('cookie-parser');
-// require('./passport'); 
-// const { createServer } = require('node:http'); // استيراد HTTP server
-// const app = express();
-
-// app.use(cookieParser());
-
-// app.use((req, res, next) => {
-//   console.log('Cookies:', req.cookies);  
-//   next();
-// });
-
-// // بقية الميدل وير الخاص بك
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-
-// // إعداد المنفذ
-// const port = process.env.PORT || 8000; 
-// const server = createServer(app); 
-
-// app.set("view engine", "ejs");
-
-
-
-// app.use(compression()); 
-// app.use(helmet()); 
-
-
-
-// إعداد الجلسات مع Passport
-// const MongoStore = require('connect-mongo');
-// app.use(session({
-//   secret: 'sessionSecret', 
-//   resave: false,         
-//   saveUninitialized: true, 
-
-//   cookie: {
-//     secure: false,      
-//     httpOnly: true,
-//     maxAge: 1000 * 60 * 60 * 24 
-//   },
-//   store: MongoStore.create({
-//     mongoUrl: process.env.MONGO_URI 
-//   })
-// }));
 const express = require("express");  //✔
 const cors = require("cors");       //✔
 const compression = require("compression"); 
@@ -68,11 +9,11 @@ const { logger } = require("./middlewares/logger");
 const { notFound, errorHandling } = require("./middlewares/errorHandler");
 const mongoose = require('mongoose');
 const passport = require('passport');
-const cookieSession = require('cookie-session');
+const session = require('express-session');
+const cookieSession=require('cookie-session')
 const cookieParser = require('cookie-parser');
 require('./passport'); 
 const { createServer } = require('node:http'); // استيراد HTTP server
-
 const app = express();
 
 app.use(cookieParser());
@@ -86,28 +27,40 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 // إعداد المنفذ
 const port = process.env.PORT || 8000; 
 const server = createServer(app); 
 
 app.set("view engine", "ejs");
 
+
 app.use(compression()); 
 app.use(helmet()); 
 
-// إعداد الجلسات مع cookie-session
-app.use(
-  cookieSession({
-    name: "session",
-    keys: [process.env.COOKIE_KEY], 
-    maxAge: 24 * 60 * 60 * 1000, // 1 يوم
+
+
+// إعداد الجلسات مع Passport
+const MongoStore = require('connect-mongo');
+app.use(session({
+  secret: 'sessionSecret', 
+  resave: false,         
+  saveUninitialized: true, 
+
+  cookie: {
+    secure: false,      
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // الجلسة تعمل على HTTPS فقط في وضع الإنتاج
+    maxAge: 1000 * 60 * 60 * 24 
+  },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI 
   })
-);
+}));
+
+
 
 app.use(cors({
-  origin: ['https://tour-relax.vercel.app', 'http://localhost:3000'],
+  origin: ['https://tour-relax.vercel.app', 'http://localhost:3000','https://linguistic-josephine-nooragniztion-eccb8a70.koyeb.app'],
   credentials: true
 }));
 
@@ -115,12 +68,14 @@ app.use(cors({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 app.use(express.static('public'));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+
 app.use(logger);
 
-// طرق رئيسية للموقع
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html')); 
 });
@@ -129,8 +84,6 @@ app.get("/", (req, res) => {
   res.send(`<h2 style="color:green;text-align:center">Welcome To Our Api App</h2>
             <h2 style="color:green;text-align:center">I will show you How To use it</h2>`);
 });
-
-// تسجيل الخروج
 app.get('/logout', (req, res) => {
   req.logout(err => {
     if (err) return next(err);
@@ -139,32 +92,40 @@ app.get('/logout', (req, res) => {
   });
 });
 
-// مسارات الـ API
 app.use("/api", require("./routes/authRoute"));
 app.use("/api", require("./routes/profileRoute"));
 app.use('/api', require("./routes/rideRoutes"));
 app.use('/api', require("./routes/citiesRoute"));
 
-// معالجات الأخطاء
+
 app.use(notFound);
 app.use(errorHandling);
 
-// بدء السيرفر
+
+
 server.listen(port, () => {
   console.log(`Server is listening on port ${port} in ${process.env.NODE_ENV} mode`);
 });
 
-// في حال كان هناك استخدام لـ Socket.io
+// // إعداد Socket.io
 // io.on('connection', (socket) => {
 //   socket.on('chat message', (msg) => {
 //     io.emit('chat message', msg);
+//     // socket.broadcast.emit('chat message',msg)
 //   });
-//   socket.on('typing', (msg) => {
-//     socket.broadcast.emit('show_typing_status');
-//   });
-//   socket.on('not_typing', (msg) => {
+//   socket.on('typing',(msg)=>{
+//     socket.broadcast.emit('show_typing_status')
+//   })
+//   socket.on('not_typing',(msg)=>{
 //     setTimeout(() => {
-//       socket.broadcast.emit('show_not_typing_status');
+//       socket.broadcast.emit('show_not_typing_status')
 //     }, 3000);
-//   });
+//   })
+  
 // });
+
+//   })
+  
+// });
+
+
